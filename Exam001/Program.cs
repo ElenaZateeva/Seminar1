530adbe7b4fc6ee693f22bed8062876471d2fab6
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-18 22:52:33 +0300

    Является ли 1е число квадратом 2го

---
+++ Exam001/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA / numberB == numberB)
+{
+    Console.WriteLine("YES");
+}
+else Console.WriteLine("NO");
