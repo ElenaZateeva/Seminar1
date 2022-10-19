ab2b565c98ab544ffbbe155b983f8e280466cd7a
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-19 08:26:34 +0300

    Наибольшее число  из 3х чисел

---
+++ Exam003/Program.cs
@@ -0,0 +1,15 @@
+﻿int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (number1 > number2) 
+    max = number1;
+else 
+max = number2;
+
+if (max < number3) 
+    max = number3;
+
+Console.Write("Наибольшее число  из 3х чисел: ");
+Console.WriteLine(max);
