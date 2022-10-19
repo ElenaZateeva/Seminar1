b0b187ec06ffc160111f0274ad0925564a090c7c
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-19 09:03:37 +0300

    Вывод последовательности четных чисел от 1 до Number

--- Exam004/Program.cs
+++ Exam004/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 > 0) 
+{
+    Console.Write("Число нечетное: ");
+    Console.WriteLine(number);
+}    
+else
+{
+    Console.Write("Число четное: ");
+    Console.WriteLine(number);
+} 
---
+++ Exam005/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+Console.Write("Последовательность четных чисел от 1 до ");
+Console.WriteLine(number);
+
+while (count < number)
+{
+    if (count % 2 == 0) 
+        Console.WriteLine(count);
+    count ++;
+}  
