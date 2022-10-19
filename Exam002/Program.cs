3a759863566f7486c2519a4875ec50d1623619b4
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-19 08:16:05 +0300

    Является ли 1е число квадратом 2го

---
+++ Exam002/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (numberA > numberB) 
+    max = numberA;
+else max = numberB;
+Console.Write("Наибольшее число: ");
+Console.WriteLine(max);
